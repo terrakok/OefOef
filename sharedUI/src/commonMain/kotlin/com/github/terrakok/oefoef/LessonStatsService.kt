358bce86fd6200e25d546fb1e836e7ce2c57700b
Oleksandr K <a.n.karpovich@gmail.com>
2026-07-03 10:53:26 +0200

    simplify

--- sharedUI/src/commonMain/kotlin/com/github/terrakok/oefoef/LessonStatsService.kt
+++ sharedUI/src/commonMain/kotlin/com/github/terrakok/oefoef/LessonStatsService.kt
@@ -4,62 +4,20 @@ import com.russhwolf.settings.Settings
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Inject
 import dev.zacsweers.metro.SingleIn
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.channels.BufferOverflow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlin.time.Duration.Companion.seconds
 
-@OptIn(FlowPreview::class)
 @SingleIn(AppScope::class)
 @Inject
 class LessonStatsService(
-    private val settings: Settings,
-    private val json: Json,
-    private val appCoroutineScope: CoroutineScope
+    private val settings: Settings
 ) {
-
-    companion object {
-        private const val LESSON_STATS_STATE_KEY = "com.github.terrakok.oefoef.saved_lesson_stats_key"
-    }
-
-    private val saveLessonStatsFlow = MutableSharedFlow<Map<String, LessonStats>>(
-        replay = 1,
-        onBufferOverflow = BufferOverflow.DROP_OLDEST
-    )
-
-    private val lessonStatsData = mutableMapOf<String, LessonStats>().also { initState ->
-        val saved = settings.getStringOrNull(LESSON_STATS_STATE_KEY)?.let { str ->
-            json.decodeFromString<Map<String, LessonStats>>(str)
-        }
-        saved?.let { initState.putAll(it) }
-    }
-
-    init {
-        appCoroutineScope.launch {
-            saveLessonStatsFlow.debounce(1.seconds).collect {
-                Log.d("Saving lessons stats")
-                settings.putString(LESSON_STATS_STATE_KEY, json.encodeToString(it))
-            }
-        }
-    }
-
     fun getLastOpenQuestionIndex(lessonId: String): Int =
-        lessonStatsData[lessonId]?.lastOpenQuestionIx ?: 0
+        settings.getInt(key(lessonId), 0)
 
     fun setLastOpenQuestionIndex(lessonId: String, questionIx: Int) {
-        val currentStats = lessonStatsData.getOrPut(lessonId) { LessonStats() }
-        lessonStatsData[lessonId] = currentStats.copy(lastOpenQuestionIx = questionIx)
-        saveLessonStatsFlow.tryEmit(lessonStatsData)
+        settings.putInt(key(lessonId), questionIx)
     }
 
+    companion object {
+        private fun key(lessonId: String) = "com.github.terrakok.oefoef.lesson_stats_${lessonId}_index"
+    }
 }
-
-@Serializable
-data class LessonStats(
-    val lastOpenQuestionIx: Int = 0
-)
